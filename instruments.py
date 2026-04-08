20e17201b91e164623f100c841e5132b42a732c5
brokeraapna <brokeraapna@gmail.com>
2026-04-08 12:56:54 +0530

    Create instruments.py

---
+++ instruments.py
@@ -0,0 +1,6 @@
+NIFTY50 = [
+    {"symbol": "RELIANCE", "key": "NSE_EQ|INE002A01018"},
+    {"symbol": "TCS", "key": "NSE_EQ|INE467B01029"},
+    {"symbol": "INFY", "key": "NSE_EQ|INE009A01021"},
+    {"symbol": "HDFCBANK", "key": "NSE_EQ|INE040A01034"},
+]
